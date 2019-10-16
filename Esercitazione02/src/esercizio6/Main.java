0c46fc3c41a66b44f5f0b8f4de792ddf908ada49
mattiazingaretti <zingaretti.mattia@gmail.com>
2019-10-16 22:16:53 +0200

    Exercise 6 done

--- Esercitazione02/src/esercizio6/Main.java
+++ Esercitazione02/src/esercizio6/Main.java
@@ -4,30 +4,41 @@ import java.util.Scanner;
 
 /**
  * Iterativly calculate a triangle area.
+ * 
  * @author zingaretti mattia
  *
  */
 public class Main {
-
+	static Scanner sc = new Scanner(System.in);
 	public static void main(String[] args) {
-		//Getting data
-		Scanner sc = new Scanner(System.in);
-		System.out.print("Insert Base: ");
-		double base = Double.parseDouble(sc.nextLine());
-		System.out.print("Insert Height : ");
-		double height = Double.parseDouble(sc.nextLine());
+		calculate();
 		sc.close();
-		
-		calculate( base, height);
 	}
 
-	private static void calculate( double base, double height) {
-		Scanner scanner = new Scanner(System.in);
-		while(base >= 0 && height >=0 ) {
-			double area = (base*height)/2;
-			System.out.print("Area : "+ area);
-		}
-		scanner.close();
+	private static double getBase() {
+		// Getting data
+		System.out.print("Insert Base: ");
+		double base = Double.parseDouble(sc.nextLine());
+		return base;
+	}
+
+	private static double getHeight() {
+		// Getting data
+		System.out.print("Insert Height : ");
+		double height = Double.parseDouble(sc.nextLine());
+		return height;
+	}
+
+
+	private static void calculate() {
+		double base;
+		double height;
+		do {
+			base = getBase();
+			height = getHeight();
+			double area = (base * height) / 2;
+			if(area>=0) {System.out.println("Area : " + area+"\n");}
+		} while (base >= 0 && height >= 0);
 	}
 
 }
