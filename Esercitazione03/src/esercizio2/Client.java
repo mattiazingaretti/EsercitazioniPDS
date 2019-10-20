1db52cd4574ed36f35731281a8d40e86496a4f54
mattiazingaretti <mattiazingaretti@192.168.1.15>
2019-10-20 15:46:08 +0200

    Test commit e push dal mac

--- Esercitazione03/src/esercizio2/Client.java
+++ Esercitazione03/src/esercizio2/Client.java
@@ -3,7 +3,7 @@ package esercizio2;
 public class Client {
 
 	public static void main(String[] args) {
-		
+		System.out.println("Aho sul mac");
 	}
 
 }
--- Esercitazione03/src/esercizio2/UtilMatrice.java
+++ Esercitazione03/src/esercizio2/UtilMatrice.java
@@ -2,7 +2,8 @@ package esercizio2;
 
 public class UtilMatrice {
 	public static double[][] differenzaVicini(double[][] m){
-		
-		return ;
+		double[][] mat = new double[1][1];
+		mat[0][0] = 1;
+		return mat;
 	}
 }
