6b538cf2ce0b970dde10b4db360e9fc8b8dff7d7
mattiazingaretti <mattiazingaretti@10.10.137.65>
2019-10-21 12:41:27 +0200

    Working on esercizio2

--- Esercitazione04/src/esercizio2/ContoCredito.java
+++ Esercitazione04/src/esercizio2/ContoCredito.java
@@ -1,5 +1,28 @@
 package esercizio2;
 
-public class ContoCredito {
+public class ContoCredito extends Conto{
+	private static final int LIMIT = 4;
+	private int commissione;
+	private static int numOP;
+	
+	public ContoCredito(String nome, String cognome, int saldo, String codice) {
+		super(nome, cognome, saldo, codice);
+		this.numOP = 0;
+		
+	}
+	@Override
+	public void deposito(int cifra) {
+		if(numOP < LIMIT) {
+			this.setSaldo(this.getSaldo() + cifra);
+			numOP++;
+		}else {
+			this.setSaldo(this.getSaldo() + cifra - this.commissione);
+			numOP++;
+		}
+	}
+	
+	public void prelievo(int cifra) {
+		this.deposito(cifra*(-1)); //lil bitch
+	}
 
 }
--- Esercitazione04/src/esercizio2/ContoDebito.java
+++ Esercitazione04/src/esercizio2/ContoDebito.java
@@ -1,5 +1,10 @@
 package esercizio2;
 
-public class ContoDebito {
+public class ContoDebito extends Conto {
+
+	public ContoDebito(String nome, String cognome, int saldo, String codice) {
+		super(nome, cognome, saldo, codice);
+		// TODO Auto-generated constructor stub
+	}
 
 }
