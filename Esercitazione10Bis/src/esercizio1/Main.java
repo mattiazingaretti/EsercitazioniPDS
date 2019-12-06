a2bd4366dfc83e4ec13b2b2639b1cec41e5d96c5
mattiazingaretti <mattiazingaretti@10.90.116.200>
2019-12-06 16:59:39 +0100

    Adding comments

--- Esercitazione10Bis/src/esercizio1/Main.java
+++ Esercitazione10Bis/src/esercizio1/Main.java
@@ -3,6 +3,11 @@ package esercizio1;
 import esercizio1.model.Model;
 import esercizio1.view.GUI;
 
+/*
+ * Client per l'esercizio mosaico. 
+ * @author Zingaretti Mattia
+ *
+ */
 public class Main {
 
 	public static void main(String[] args) {
